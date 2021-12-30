26d9d5788b66426264c1fc064d53e9484d91cb94
hardyvince <hardy.vince@gmail.com>
2021-12-30 01:41:24 +0100

    Base script for free host trigger(to keep host) and backup

---
+++ bash/daily_downdata.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo start
+
+while getopts d:o:p: flag
+do
+    case "${flag}" in
+            d) domain=${OPTARG};;
+            o) other=${OPTARG};;
+            p) project=${OPTARG};;
+    esac
+done
+#echo "Username: $username";
+
+python sync.py -d $domain -o $other -m downdata -p $project
+
+res1=($(ls -1t | grep database.zip))
+
+if [[ `diff   <(unzip -vqq ${res1[1]} | awk '{$2=""; $3=""; $4=""; $5=""; $6=""; print}' | sort -k3)   <(unzip -vqq ${res1[0]} | awk '{$2=""; $3=""; $4=""; $5=""; $6=""; print}' | sort -k3)` != "" ]]; then echo "different"; if [[ ${#res1[@]} > 3 ]]; then rm ${res1[-1]}; fi; else echo "equvivalent"; rm ${res1[0]}; fi
